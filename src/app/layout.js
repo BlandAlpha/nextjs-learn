e3259cf5a69bd1032ea7848473de88d6caec3bda
CanisAlpha <760097576@qq.com>
2025-02-25 15:03:06 +0800

    font change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,15 @@
-import { Mulish } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 
-const mulish = Mulish({ subsets: ['latin'] })
+const dm_sans = DM_Sans({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={cn('min-h-screen font-sans antialiased', mulish.className)}
+        className={cn('min-h-screen font-sans antialiased', dm_sans.className)}
       >
         <Navbar />
         {children}
